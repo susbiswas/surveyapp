f2eef942db41ed1cd72e9f9128028eaaaa36ef5c
susbiswas <susmita.biswas006@gmail.com>
2021-03-13 22:38:22 -0500

    Chart View

--- chartapp/views.py
+++ chartapp/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import JsonResponse
 
 # Create your views here.
+def get_data(request, *args, **kwargs):
+    labels = ["Users", "Blue", "Yellow", "Green", "Purple", "Orange"]
+    default_items = [10, 23, 2, 3, 12, 2]
+    data = {
+            "labels": labels,
+            "default": default_items,
+    }
+    return JsonResponse(data)
