dca2f39258859d789fae90c0090871d555704a7c
susbiswas <susmita.biswas006@gmail.com>
2021-03-24 10:08:03 -0400

    updated admin.py to add data from admin end

--- chartapp/admin.py
+++ chartapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Vaccination_Status,HealthIssue,Prevaccine_Health_Issue,Postvaccine_Health_Issue
+
+admin.site.register(Vaccination_Status)
+admin.site.register(HealthIssue)
+admin.site.register(Prevaccine_Health_Issue)
+admin.site.register(Postvaccine_Health_Issue)
